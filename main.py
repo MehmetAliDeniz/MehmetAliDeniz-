53a84fe017191d1655b669477e38bc98f9b72c65
MehmetAliDeniz <mehmetali.deniz@std.yeditepe.edu.tr>
2024-12-26 17:31:54 +0300

    MehmetAliDeniz

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-alter = int(input("alter="))
-note = int(input("Note="))
-if 20<=alter <=50 and note >80:
+punktzahl=float(input("punktzahl:"))
+programmierefahrung=int(input("programierefahrung(1-5)"))
+
+if punktzahl > 90 or(programierefahrung == 5 and punktzahl >= 70):
   print("einstellen")
+elif punkzahl >= 70 or programmierefahrung == 4:
+   primt("zum Gesprach einladen")
 else:
   print("ablehnen")
